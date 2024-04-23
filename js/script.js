4262a5a842b56af8508d02534a88391d4ab5cd18
Alex <lev_x@ukr.net>
2024-04-23 20:11:16 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,65 @@
+"use strict";
+
+//007 простое общение с пользователем
+
+//Предупреждение 
+//alert("Hello");
+
+//Вопрос с ответом да или нет
+/*const result = confirm("Are you here?");
+console.log(result);*/
+
+//Вопрос с ответом, которое пользователь вводить самостоятельно
+/*const answer = prompt ("Вам есть 18+?", "18");
+console.log(answer);*/
+
+//Массив с ответами от пользователя
+/*
+const answers = [];
+
+answers[0] = prompt('Как Ваше имя?', '');
+answers[1] = prompt('Как Ваша фамилия?', '');
+answers[2] = prompt('Сколько Вам лет?', '');
+
+document.write(answers);
+*/
+
+//008 Интерполяция (соединение строк)
+/*
+const user = "Alex";
+alert(`Привет, ${user}`);
+*/
+
+//009 Операторы
+
+let incr = 10,
+	decr = 10;
+
+//инкремент увеличение на 1
+incr++;
+//декремент уменьшение на 1
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
